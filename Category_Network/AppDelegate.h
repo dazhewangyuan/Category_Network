5859fde87e2fcf138df33893db318ad680738437
dazhewangyuan <dazhewangyuan@163.com>
2017-07-13 13:50:52 +0800

    Initial Commit

---
+++ Category_Network/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Category_Network
+//
+//  Created by long on 2017/7/13.
+//  Copyright © 2017年 WZYK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
